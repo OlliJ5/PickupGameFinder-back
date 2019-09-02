464237348a1d3b446ad5f9fcc011e69f829c6367
Olli Rousu <olli.rousu@hotmail.com>
2019-09-02 15:21:44 +0300

    started creating a skeleton for the app

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('moro toimii')
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+    console.log(`Server running on port ${config.PORT}`)
+})
+
