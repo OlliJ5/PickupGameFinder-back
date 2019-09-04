032e4fcaacd461043f0f4078f3c8bc97eb23e14b
Olli Rousu <olli.rousu@hotmail.com>
2019-09-04 17:50:25 +0300

    backend now allows players to join games

--- controllers/activeGames.js
+++ controllers/activeGames.js
@@ -12,9 +12,6 @@ activeGamesRouter.post('/', async (request, response, next) => {
   const body = request.body
   const token = request.token
 
-  console.log('duration', body.duration)
-  console.log('token', token)
-
   try {
     const decodedToken = jwt.verify(token, process.env.SECRET)
 
@@ -38,6 +35,36 @@ activeGamesRouter.post('/', async (request, response, next) => {
 
     const savedGame = await activeGame.save()
     response.json(savedGame)
+  } catch (exception) {
+    next(exception)
+  }
+})
+
+
+//This can be used to add a player to the game. The added player is recognized with the token
+activeGamesRouter.put('/:id', async (request, response, next) => {
+  const token = request.token
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    const activeGame = await ActiveGame.findById(request.params.id)
+
+    //checks if the player is already a participant in the game
+    if (activeGame.participants.includes(user._id)) {
+      return response.status(400).json({ error: 'player is already in the game' })
+    }
+    activeGame.participants = activeGame.participants.concat(user._id)
+    await activeGame.save()
+
+    response.json(activeGame)
+
   } catch (exception) {
     next(exception)
   }
